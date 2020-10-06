ae07f3de15029a4b73be088b7b6fe71a30bf594a
Fergus Yip <fergus.yipwailam@gmail.com>
2020-10-06 20:34:07 +1100

    Remove consolelog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import Board from "./components/Board";
-import Header from "./components/Header";
-import Game2048, { isSameState, updateState, GAME_STATES } from "./Game2048";
-import useLocalStorage from "./hooks/useLocalStorage";
-import { useSwipeable } from "react-swipeable";
-import Overlay from "./components/Overlay";
+import React, { useEffect } from 'react';
+import './App.css';
+import Board from './components/Board';
+import Header from './components/Header';
+import Game2048, { isSameState, updateState, GAME_STATES } from './Game2048';
+import useLocalStorage from './hooks/useLocalStorage';
+import { useSwipeable } from 'react-swipeable';
+import Overlay from './components/Overlay';
 
 function App() {
   const [gameState, setGameState] = useLocalStorage(
-    "gameState",
+    'gameState',
     Game2048.newGame()
   );
-  const [bestScore, setBestScore] = useLocalStorage("bestScore", 0);
+  const [bestScore, setBestScore] = useLocalStorage('bestScore', 0);
   const swipeHandlers = useSwipeable({
-    onSwipedUp: () => setGameState((current) => getNextState("up", current)),
+    onSwipedUp: () => setGameState((current) => getNextState('up', current)),
     onSwipedDown: () =>
-      setGameState((current) => getNextState("down", current)),
+      setGameState((current) => getNextState('down', current)),
     onSwipedLeft: () =>
-      setGameState((current) => getNextState("left", current)),
+      setGameState((current) => getNextState('left', current)),
     onSwipedRight: () =>
-      setGameState((current) => getNextState("right", current)),
+      setGameState((current) => getNextState('right', current)),
   });
 
   const restartGame = () => {
@@ -30,16 +30,16 @@ function App() {
   const getNextState = (direction, current) => {
     let result = current;
     switch (direction.toLowerCase()) {
-      case "up":
+      case 'up':
         result = Game2048.nextUpState(result);
         break;
-      case "down":
+      case 'down':
         result = Game2048.nextDownState(result);
         break;
-      case "left":
+      case 'left':
         result = Game2048.nextLeftState(result);
         break;
-      case "right":
+      case 'right':
         result = Game2048.nextRightState(result);
         break;
       default:
@@ -56,21 +56,20 @@ function App() {
 
   useEffect(() => {
     const handleKeyPress = (e) => {
-      console.log(e.key);
       if (gameState.state !== GAME_STATES.PLAYING) {
-        if (e.key === "Enter") {
+        if (e.key === 'Enter') {
           setGameState(Game2048.newGame());
         }
         return;
       }
       setGameState((current) =>
-        getNextState(e.key.replace("Arrow", ""), current)
+        getNextState(e.key.replace('Arrow', ''), current)
       );
     };
 
-    document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener('keydown', handleKeyPress);
     return () => {
-      document.removeEventListener("keydown", handleKeyPress);
+      document.removeEventListener('keydown', handleKeyPress);
     };
   }, [gameState, setGameState]);
 
@@ -87,7 +86,7 @@ function App() {
       <Board tiles={gameState.tiles} />
       <Overlay
         display={gameState.state !== GAME_STATES.PLAYING}
-        title={gameState === GAME_STATES.WIN ? "Victory" : "Game Over"}
+        title={gameState === GAME_STATES.WIN ? 'Victory' : 'Game Over'}
         subtitle="Click to restart"
         onClick={restartGame}
       />
